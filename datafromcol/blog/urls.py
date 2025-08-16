28b2217ddc87b6e6ff83d42295159d479725e6c5
ElGalileo12 <kevingalileo100@gmail.com>
2025-08-16 18:37:04 -0500

    feat: add html and tailwind

--- datafromcol/blog/urls.py
+++ datafromcol/blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import post_list
+from . import views
 
 urlpatterns = [
-    path("", post_list, name="post_list"),
+    path("", views.inicio, name="inicio"),
+    path("list", views.post_list, name="post_list"),
 ]
--- datafromcol/blog/views.py
+++ datafromcol/blog/views.py
@@ -5,3 +5,8 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.order_by("-created_at")
     return render(request, "blog/post_list.html", {"posts": posts})
+
+
+def inicio(request):
+    contexto = {"nombre": "Home Page"}
+    return render(request, "home/home.html", contexto)
